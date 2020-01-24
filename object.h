8e337599a0b08cf318dc46eff6a95cbcdbc48412
j-Myles <42116741+j-Myles@users.noreply.github.com>
2020-01-24 15:10:44 -0500

    Added object.h

---
+++ object.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <stdbool.h>
+
+/**
+ * @brief Parent Object Class
+ * 
+ */
+class Object {
+public:
+
+	/**
+	 * @brief checks if the given object is equivalent to this object
+	 * 
+	 * @param o given object to be compared
+	 * @return true if equivalent
+	 * @return false if non-equivalent
+	 */
+	bool equals(Object o);
+
+	/**
+	 * @brief duplicates object with identical attributes
+	 * 
+	 * @return Object cloned object
+	 */
+	Object copy();
+
+	/**
+	 * @brief computes unique indentity-corresponding integer for object
+	 * 
+	 * @return int hash value
+	 */
+	int hashcode();
+
+};
